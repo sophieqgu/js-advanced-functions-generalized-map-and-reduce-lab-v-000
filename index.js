86fc038df59b43e51e6d00eee48a5ec0afa42724
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 02:39:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your functions here
+function map(sourceArray, callback) {
+  let result = [];
+  for (let i = 0; i < sourceArray.length; i++) {
+    result.push(callback(sourceArray[i]));
+  }
+  return result;
+}
+
+function reduce(sourceArray, callback, startValue) {
+  let result = (!!startValue) ? startValue : sourceArray[0];
+  let i = (!!startValue) ? 0 : 1;
+  for (i; i < sourceArray.length; i++) {
+    result = callback(sourceArray[i], result);
+  }
+  return result;
+}
