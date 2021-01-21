f4389a8fc0c2b7aee5376bdf3a071cd5ad55097a
Sophie Gu <sophieqgu@gmail.com>
2021-01-21 02:16:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ function map(sourceArray, callback) {
   for (let i = 0; i < sourceArray.length; i++) {
     result.push(callback(sourceArray[i]));
   }
+  return result;
+}
+
+function reduce(sourceArray, callback) {
+  
 }
